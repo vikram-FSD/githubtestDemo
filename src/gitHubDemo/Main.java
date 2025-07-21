539fdc2d9c247575ced17a32444ff5ccc66433fe
JanakiKannappan <janaki.k@atomedgesoft.com>
2025-07-21 12:28:49 +0530

    Attendance details added

--- src/gitHubDemo/Main.java
+++ src/gitHubDemo/Main.java
@@ -4,11 +4,16 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		Student.Grade();
+		Student.Attendance();
 	}
 	
 	public class Student{
 		public static void Grade() {
 			System.out.println("Grade is 'D'");
+		}
+		
+		public static void Attendance() {
+			System.out.println("Your attendance percentage is: 82%");
 		}
 	}
 }
